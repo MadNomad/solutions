85915536aad660043755e428211b9c1888380976
Mad Nomad <mad.nomad.ua@gmail.com>
2016-01-18 22:04:09 +0200

    ...

--- Figure/Figure.cs
+++ Figure/Figure.cs
@@ -8,15 +8,48 @@ namespace Figure
 {
     class Figure
     {
-        public Figure(Point A, Point B, Point C) { }
-        public Figure(Point A, Point B, Point C, Point D) { }
-        public Figure(Point A, Point B, Point C, Point D, Point E) { }
+        public List<Point> points; 
+
+        public Figure(Point A, Point B, Point C)
+        {
+            points.Add(A);
+            points.Add(B);
+            points.Add(C);
+            
+            PerimeterCalculator(points);
+        }
+
+        public Figure(Point A, Point B, Point C, Point D)
+        {
+            Point a = A;
+            Point b = B;
+            Point c = C;
+            Point d = D;
+            int numOfPoints = 4;
+        }
+
+
+        public Figure(Point A, Point B, Point C, Point D, Point E)
+        {
+            Point a = A;
+            Point b = B;
+            Point c = C;
+            Point d = D;
+            Point e = E;
+            int numOfPoints = 5;
+        }
 
         double LengthSide(Point A, Point B)
         {
             double length = Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
             return length;
         }
-        void PerimeterCalculator() { }
+
+
+        void PerimeterCalculator(List<Point> points)
+        {
+            Console.WriteLine("Точка с координатами X=" + points[0].X + " Y=" + points[0].Y);
+        }
+
     }
 }
--- User/Program.cs
+++ User/Program.cs
@@ -4,16 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Figure
+namespace User
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Point a = new Point(1, 1, "A");
-            Point b = new Point(2, 2, "B");
-            Point c = new Point(3, 2, "C");
-            Figure newFigure = new Figure(a, b, c);
         }
     }
 }
