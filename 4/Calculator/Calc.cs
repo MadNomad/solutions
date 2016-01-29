e6845eb7126e3e6deb4080e6893d6be9832eccae
Mad Nomad <mad.nomad.ua@gmail.com>
2016-01-29 16:51:23 +0200

    4-1

---
+++ 4/Calculator/Calc.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by Mad Nomad
+ * User: Sergey Ermolov
+ * Date: 29.01.2016
+ * Time: 15:10
+ */
+using System;
+
+namespace Calculator
+{
+	/// <summary>
+	/// Description of Calc.
+	/// </summary>
+	public static class Calc
+	{
+		static double var1, var2;
+		
+		public static void Summ (double var1, double var2)
+		{
+			Console.WriteLine("{0} + {1} = " + (var1 + var2), var1, var2);
+		}
+
+		public static void Razn (double var1, double var2)
+		{
+			Console.WriteLine("{0} - {1} = " + (var1 - var2), var1, var2);
+		}
+
+		public static void Umn (double var1, double var2)
+		{
+			Console.WriteLine("{0} * {1} = " + (var1 * var2), var1, var2);
+		}
+
+		public static void Delen (double var1, double var2)
+		{
+			Console.WriteLine("{0} / {1} = " + (var1 / var2), var1, var2);
+		}
+	}
+}
---
+++ 4/Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by Mad Nomad
+ * User: Sergey Ermolov
+ * Date: 29.01.2016
+ * Time: 12:55
+ */
+using System;
+
+namespace Calculator
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			Calc.Summ(3,3);
+			Calc.Razn(3,3);
+			Calc.Umn(3,3);
+			Calc.Delen(3,3);
+			
+			Console.WriteLine();
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
