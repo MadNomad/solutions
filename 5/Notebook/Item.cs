5c8757ca23ad580ae4119b1b23a758fedc418c3f
Mad Nomad <mad.nomad.ua@gmail.com>
2016-02-02 14:14:05 +0200

    5

---
+++ 5/Notebook/Item.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by Mad Nomad
+ * User: Sergey Ermolov
+ * Date: 02.02.2016
+ * Time: 11:20
+ */
+using System;
+
+namespace Notebook
+{
+	/// <summary>
+	/// Создайте структуру с именем - Notebook.
+	/// Поля структуры: модель, производитель, цена.
+	/// В структуре должен быть реализован конструктор для инициализации полей
+	/// </summary>
+	public struct Item
+	{
+		public string model;
+		public string manufacturer;
+		public double price;
+
+		public Item (string model, string manufacturer, double price)
+		{
+			this.model = model;
+			this.manufacturer = manufacturer;
+			this.price = price;
+		}
+		
+		public void Show()
+        {
+            Console.WriteLine("{0}\t\t{1}\t\t\t{2}$", manufacturer, model, price);
+        }
+
+	}
+}
---
+++ 5/Notebook/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by Mad Nomad
+ * User: Sergey Ermolov
+ * Date: 02.02.2016
+ * Time: 11:09
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Notebook
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			List<Item> database = new List<Item>();
+			database.Add(new Item("G550", "Lenovo", 440));
+			database.Add(new Item("G560", "Lenovo", 540));
+			database.Add(new Item("G580", "Lenovo", 640));
+			database.Add(new Item("Y560", "Lenovo", 840));
+			database.Add(new Item("Y580", "Lenovo", 940));
+			
+			foreach(Item item in database)
+			{
+				item.Show();
+			}
+			
+			Console.WriteLine("\nPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
