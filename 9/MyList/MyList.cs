2e4dbeaaaba75f0515ddb2caeee50ae07cf72244
Mad Nomad <mad.nomad.ua@gmail.com>
2016-02-11 17:34:57 +0200

    9

---
+++ 9/MyList/MyList.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by Mad Nomad
+ * User: Sergey Ermolov
+ * Date: 09.02.2016
+ * Time: 16:41
+ */
+using System;
+
+namespace MyList
+{
+	/// <summary>
+	/// Description of MyList.
+	/// </summary>
+	public class MyList : IEquatable, IEnumerator
+	{
+		public MyList()
+		{
+		}
+	}
+}
---
+++ 9/MyList/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by Mad Nomad
+ * User: Sergey Ermolov
+ * Date: 09.02.2016
+ * Time: 15:58
+ */
+using System;
+
+namespace MyList
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
